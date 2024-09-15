42f34445cdc11f38aca2faa8d4f5f4f7f6e6eac0
Addison Beck <github@addisonbeck.com>
2024-09-14 20:12:57 -0400

    .

--- binwarden.nix
+++ binwarden.nix
@@ -1,24 +1,17 @@
-{ stdenv, pkgs }:
+{ stdenv }:
 
 let version = "0.0.1";
 in stdenv.mkDerivation {
 
-  name = "d-${version}";
+  name = "binwarden-${version}";
 
   src = ./src;
 
-  nativeBuildInputs = [ ];
-  buildInputs = [ pkgs.makeWrapper ];
-
-  buildPhase = "";
+  phases = [ "unpackPhase" "installPhase" ];
 
   installPhase = ''
-    mkdir -p $out/binwarden/
-    cp -r $src $out/binwarden
-    install -D $src/bin/b $out/binwarden/bin/b
-  '';
-
-  postFixup = ''
-     wrapProgram $out/binwarden/bin/b --prefix PATH : $out/bin
+    mkdir -p $out/bin
+    cp -r $src/lib $out/
+    install -D $src/bin/binwarden $out/bin
   '';
 }
